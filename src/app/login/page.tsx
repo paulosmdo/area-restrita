51e07b1b404aeaaa0ce53571fa444c615dc277cb
Paulo Sérgio <paulo.sergio@tecsoft.info>
2024-11-29 17:21:00 -0300

    fix: login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { LogInIcon } from "lucide-react";
+import { Eye, EyeOff, LogInIcon } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+// import { useState } from "react";
 
+// const [showPassword, setShowPassword] = useState(false);
 const Page = () => {
+    
     return ( 
     <div className="grid grid-cols-2 h-full">
         <div className="relative h-full w-full bg-gradient-to-r from-orange-400 to-orange-600">
@@ -17,7 +21,7 @@ const Page = () => {
                 <h1 className="mb-3 text-4xl font-bold text-orange-600">Bem-Vindo</h1>
 
                 <Label className="block text-sm font-medium text-gray-700 mt-4 mb-1">E-mail: </Label>
-                <Input className="mb-2" id="email"
+                <Input className="mb-2 placeholder:font-bold placeholder:text-gray-400" id="email"
                     placeholder="Digite seu e-mail"
                     type="email"
                     autoCapitalize="none"
@@ -25,15 +29,26 @@ const Page = () => {
                     autoCorrect="off"/>
 
                 <Label className="block text-sm font-medium text-gray-700 mt-4 mb-1">Senha: </Label>
-                <Input className="mb-2" id="senha"
+                <Input className="mb-2 placeholder:font-bold placeholder:text-gray-400" id="senha"
                     placeholder="Digite sua senha"
                     type="password"
                     autoCapitalize="none"
                     />
+                {/* <button
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="absolute right-2 top-2.5 text-gray-500 hover:text-gray-700 focus:outline-none"
+                >
+                    {showPassword ? <EyeOff className="w-5 h-5" /> : <Eye className="w-5 h-5" />}
+                </button> */}
 
-                <Button className="w-full mt-6 h-12">
+                <div className="w-full text-right" >
+                    <Link href="/login" className="text-orange-500 font-bold">Esqueceu sua senha?</Link>
+                </div>
+
+                <Button className="w-full mt-8 h-12 font-bold">
                     <LogInIcon className="mr-2"/>
-                    Fazer login
+                    Acessar
                 </Button>
             </div>
         </div>
